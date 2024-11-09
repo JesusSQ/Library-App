5b81e9ff15bbca51cda8ea3d6660896eae907c8a
Jesus Sanchez <jesus04122@gmail.com>
2024-11-09 20:25:10 +0100

    New version using classes instead of constructors

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 // Const and variables
 
 let index = 0 // Id asign
-const myLibrary = [] // Book storage
-let deleteButtons = []
-let toggleButtons = []
+const bookArray = [] // Book storage
 
 // Selectors
 
@@ -16,96 +14,97 @@ const pagesInput = dialog.querySelector('#pages')
 const readInput = dialog.querySelector('#read')
 const bookForm = dialog.querySelector('form')
 
-// Constructor
+// Book class
 
-function Book(title, author, pages, read) {
-    this.id = setId()
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function() {
-        console.log(this.title + ' by ' + this.author + ', ' + this.pages + ' pages, ' + this.read)
+class Book {
+    constructor(title, author, pages, read) {
+        this.id = this.setId()
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read
+    }
+
+    setId() {
+        const id = index
+        index += 1
+        return id
+    } 
+}
+
+// Library class
+
+class Library {
+    constructor (books){
+        this.books = books
+    }
+
+    addBookToLibrary() {
+        const title = titleInput.value
+        const author = authorInput.value
+        const pages = pagesInput.value
+        const read = readInput.checked
+            ? 'already read'
+            : 'not read' 
+    
+        const newBook = new Book(title, author, pages, read)
+    
+        this.books.push(newBook)
+        bookForm.reset()
+    
+        this.displayBooks()
+    }
+
+    removeBook(id) {
+        const position = this.books.findIndex(book => book.id == id)
+        this.books.splice(position, 1)
+        this.displayBooks()
+    }
+
+    toggleBook(id) {
+        const book = this.books.find(book => book.id == id)
+        book.read = book.read === 'already read' ? 'not read' : 'already read'
+        this.displayBooks()
+    }
+
+    displayBooks() {
+        cont.innerHTML = ''
+        for (const book of this.books) {
+            const e = document.createElement('div')
+            e.classList.add('card')
+            e.innerHTML = `
+                <h3>${book.title}</h3>
+                <div class='text'>
+                    <p>Author: ${book.author}</p>
+                    <p>Pages: ${book.pages}</p>
+                    <p>Status: ${book.read}</p>
+                </div>
+                <div id='buttonContainer'>
+                    <button class='delete' id='${book.id}'>Remove</button>
+                    <button class='toggle' id='${book.id}'>Toggle</button>
+                </div>`
+            cont.appendChild(e)
+        }
+
+        let deleteButtons = document.querySelectorAll('.delete')
+        let toggleButtons = document.querySelectorAll('.toggle')
+        
+        deleteButtons.forEach(function(button) {
+            button.addEventListener('click', function() {
+                myLibrary.removeBook(button.id)
+            })
+        })
+        
+        toggleButtons.forEach(function(button) {
+            button.addEventListener('click', function() {
+                myLibrary.toggleBook(button.id)
+            })
+        })
+
     }
 }
 
-function setId() {
-    const id = index
-    index += 1
-    return id
-} 
-
-// Add new book to the library
-
-function addBookToLibrary() {
-    const title = titleInput.value
-    const author = authorInput.value
-    const pages = pagesInput.value
-    const read = readInput.checked
-        ? 'already read'
-        : 'not read' 
-
-    const newBook = new Book(title, author, pages, read)
-
-    myLibrary.push(newBook)
-    bookForm.reset()
-
-    displayBooks()
-}
-
-// Remove a book from the library
-
-function removeBook(id) {
-    const position = myLibrary.findIndex(book => book.id == id)
-    myLibrary.splice(position, 1)
-    displayBooks()
-}
-
-// Toggle status of a book
-
-function toggleBook(id) {
-    const book = myLibrary.find(book => book.id == id)
-    book.read = book.read === 'already read' ? 'not read' : 'already read'
-    displayBooks()
-}
-
-// Display the existing books in the library
-
-function displayBooks() {
-    cont.innerHTML = ''
-    for (const book of myLibrary) {
-        const e = document.createElement('div')
-        e.classList.add('card')
-        e.innerHTML = `
-            <h3>${book.title}</h3>
-            <div class='text'>
-                <p>Author: ${book.author}</p>
-                <p>Pages: ${book.pages}</p>
-                <p>Status: ${book.read}</p>
-            </div>
-            <div id='buttonContainer'>
-                <button class='delete' id='${book.id}'>Remove</button>
-                <button class='toggle' id='${book.id}'>Toggle</button>
-            </div>`
-        cont.appendChild(e)
-        deleteButtons = document.querySelectorAll('.delete')
-        toggleButtons = document.querySelectorAll('.toggle')
-    }
-
-    // Event listeners (card buttons)
-
-    deleteButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            removeBook(button.id)
-        })
-    })
-
-    toggleButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            toggleBook(button.id)
-        })
-    })
-}
+const myLibrary = new Library(bookArray)
 
 // Event listeners
 
@@ -115,6 +114,6 @@ createBook.addEventListener('click', () => {
 
 bookForm.addEventListener('submit', (e) => {
     e.preventDefault() // Avoid problems with form information
-    addBookToLibrary()
+    myLibrary.addBookToLibrary()
     dialog.close()
 })
